29d8d4404fb3a8a4a8903e5942592cbd5033a76f
João Pedro Vendramini <joao.vendramini@muralis.com.br>
2022-01-06 22:10:59 -0300

    project started

---
+++ src/Components/ImageFilter.js
@@ -0,0 +1,19 @@
+import React from "react"
+import alimentacao from '../assets/images/alimentacao.svg';
+import outros from '../assets/images/outros.svg';
+import saude from '../assets/images/saude.svg';
+import transporte from '../assets/images/transporte.svg';
+import utilidades from '../assets/images/utilidades.svg';
+import { IconeTema } from "./UI";
+
+export default (type) => {
+    const Images = {
+        Restaurante: <IconeTema src={alimentacao} alt="Restaurante"/>,
+        Saude: <IconeTema src={saude} alt="Saude"/>,
+        Transporte: <IconeTema src={transporte} alt="transporte"/>,
+        Utilidades: <IconeTema src={utilidades} alt="utilidades"/>,
+        default: <IconeTema src={outros} alt="outros"/>
+    };
+
+    return Images[type] || Images.default;
+};
